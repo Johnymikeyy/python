eb8f8d6994dbf683bd420458a04730a1ce902df1
John <johnymikeyy@gmail.com>
2021-01-13 16:25:44 +0100

    some works

--- workshop/workshop.py
+++ workshop/workshop.py
@@ -1,11 +1,18 @@
+
+max = a
+if b > max:
+   max = b
+   print(max)
+   
+number = int(input('Please enter a number: '))
+i = 0
+while i in range(0, number):
+    print(i**2)
+    i += 1
+    
 sample_list = [{"section":5, "topic":2}, 'clarusway', [1, 4], 2020, 3.14, 1+618j, False, (10, 20)]
 
-for item in sample_list:
-    #print(f"The type of {sample_list[]} is {type(sample_list[])}")
+for i in sample_list:
+   print(f"The type of {i} is {type(i)}")
 
 
- word = input("Enter a word:")
- seperator = input("Enter a seperator:")
- repetition = input("How many times you want it to repeat:")
-
- print(*list([word] * int(repetition)), sep=seperator)
