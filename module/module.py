d17e149aa4efdb38b7718811ff8560e2ae2abd63
John <johnymikeyy@gmail.com>
2021-01-28 16:42:55 +0100

    random

---
+++ module/module.py
@@ -0,0 +1,13 @@
+def my_function_1(a, b):
+    area = a * b
+    return area
+
+def my_function_2(a, b):
+    hypotenuse = (a**2 + b**2)**0.5
+    return hypotenuse
+
+
+import module
+
+area = module.my_function_1(10, 20)
+hypotenuse = module.my_function_2(30, 40)
--- workshop/empty.py
+++ workshop/empty.py
@@ -2,3 +2,9 @@
 word = input("Enter a word:")
 print(*list([word]))
 
+
+#########
+fruits = {'Apples': 5, 'Oranges': 3, 'Bananas': 4}
+
+fruit_names = [x for x in fruits.keys()]
+print(fruit_names)
