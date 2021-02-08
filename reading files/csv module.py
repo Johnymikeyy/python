671808cdae9eccdae4129303b23e6d5e93de37e3
fgok <johnymikeyy@gmail.com>
2021-02-08 23:10:44 +0100

    csv module

---
+++ reading files/csv module.py
@@ -0,0 +1,8 @@
+#reading the csv files with csv Module
+
+import csv
+with open("fruits.csv", 'r', newline ='', encoding = 'utf-8') as file:
+    csv_rows = csv.reader(file)
+    
+    for fow in csv_rows:
+        print(row)
